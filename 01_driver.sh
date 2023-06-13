e4e10d53140944bedb4641e260f36569dfa8b103
Gerard Mortel <gmortel@us.ibm.com>
2023-06-13 00:01:26 -0500

    Updated to reflect renamed file

--- 01_driver.sh
+++ 01_driver.sh
@@ -2,7 +2,7 @@
 
 . ./02_setup_env.sh
 ./03_install_cloudctl.sh
-./04_install_baw_catalog_source_and_operator.sh
+./04_install_baw_pfs_fncm_operators.sh
 
 echo ""
 echo "================================================================================"
