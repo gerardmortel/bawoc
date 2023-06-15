e2c7f85bc52c6be1b295759658d08755e3b0ed7e
Gerard Mortel <gmortel@us.ibm.com>
2023-06-14 22:32:55 -0500

    Added property generation

--- 01_driver.sh
+++ 01_driver.sh
@@ -9,3 +9,4 @@ echo ""
 . ./02_setup_env.sh
 ./03_install_cloudctl.sh
 ./04_install_baw_pfs_fncm_operators.sh
+./05_baw_prerequisites_property.sh
---
+++ 05_baw_prerequisites_property.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo ""
+echo "================================================================================"
+echo "=== In 05_baw_prerequisites_property.sh ========================================"
+echo "================================================================================"
+echo ""
+
+cd ibm-cs-bawautomation/inventory/cp4aOperatorSdk/files/deploy/crs/cert-kubernetes/scripts
+# Generate property files
+./baw-prerequisites.sh -m property
+
+cd baw-prerequisites/propertyfile
+
+# Edit property files
