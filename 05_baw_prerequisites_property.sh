2a7e361bc82d2843f3041738d51960a6c91089d0
Gerard Mortel <gmortel@us.ibm.com>
2023-06-16 09:52:06 -0500

    Added values needed to make property files

--- 05_baw_prerequisites_property.sh
+++ 05_baw_prerequisites_property.sh
@@ -9,3 +9,13 @@ echo ""
 cd ibm-cs-bawautomation/inventory/cp4aOperatorSdk/files/deploy/crs/cert-kubernetes/scripts
 # Generate property files
 ./baw-prerequisites.sh -m property
+# 2
+# 2
+# 1
+# 2
+# 1
+# bpmdb,bawaudb,umsdb,appdb,aaedb,aeosdb,bawtos,bawdos,bawdocs,os1db,icndb,gcddb
+# BPMDB,BAWAUDB,UMSDB,APPDB,AAEDB,AEOSDB,BAWTOS,BAWDOS,BAWDOCS,OS1DB,ICNDB,GCDDB
+# nfs-managed-storage
+# nfs-managed-storage
+# nfs-managed-storage
