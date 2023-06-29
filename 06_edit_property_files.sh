e564a86963ef7384a4cea4daa733c877244d0b33
Gerard Mortel <gmortel@us.ibm.com>
2023-06-28 22:22:32 -0500

    Corrected baw_db_server.property section

--- 06_edit_property_files.sh
+++ 06_edit_property_files.sh
@@ -47,16 +47,17 @@ cp baw_db_name_user.property.11 baw_db_name_user.property
 # baw_db_server.property
 cp -p baw_db_server.property baw_db_server.property.orig
 # %s/SERVERNAME=\"<Required>\"/SERVERNAME=\"c-db2ucluster-cp4ba-db2u.cp4ba.svc.cluster.local\"/g
-sed -r "s/SERVERNAME=\"<Required>\"/SERVERNAME=\"c-db2ucluster-cp4ba-db2u.cp4ba.svc.cluster.local\"/g baw_db_server.property > baw_db_server.property.1
+sed -r "s/SERVERNAME=\"<Required>\"/SERVERNAME=\"c-db2ucluster-cp4ba-db2u.cp4ba.svc.cluster.local\"/g" baw_db_server.property > baw_db_server.property.1
 # %s/PORT=\"<Required>\"/PORT=\"50000\"/g
-sed -r "s/PORT=\"<Required>\"/PORT=\"50000\"/g baw_db_server.property.1 > baw_db_server.property.2
+sed -r "s/PORT=\"<Required>\"/PORT=\"50000\"/g" baw_db_server.property.1 > baw_db_server.property.2
 # %s/SSL_ENABLE=\"True\"/SSL_ENABLE=\"false\"/g
 sed -r "s/SSL_ENABLE=\"True\"/SSL_ENABLE=\"false\"/g" baw_db_server.property.2 > baw_db_server.property.3
 # %s/SERVERNAME=\"<Optional>\"/SERVERNAME=\"\"/g
 sed -r "s/SERVERNAME=\"<Optional>\"/SERVERNAME=\"\"/g" baw_db_server.property.3 > baw_db_server.property.4
 # %s/PORT=\"<Optional>\"/PORT=\"\"/g
-sed -r "s/PORT=\"<Optional>\"/PORT=\"\"/g"
-rm -f 
+sed -r "s/PORT=\"<Optional>\"/PORT=\"\"/g" baw_db_server.property.4 > baw_db_server.property.5
+rm -f baw_db_server.property
+cp baw_db_server.property.5 baw_db_server.property
 
 # Edit the baw_LDAP.property
 cp -p baw_LDAP.property baw_LDAP.property.orig
@@ -64,7 +65,7 @@ sed -r 's|LDAP_SERVER="<Required>"|LDAP_SERVER="openldap.cp4ba.svc.cluster.local
 sed -r 's|LDAP_PORT="<Required>"|LDAP_PORT="389"|g' baw_LDAP.property.1 > baw_LDAP.property.2
 sed -r 's|LDAP_BASE_DN="<Required>"|LDAP_BASE_DN="dc=your,dc=company,dc=com"|g' baw_LDAP.property.2 > baw_LDAP.property.3
 sed -r 's|LDAP_BIND_DN="<Required>"|LDAP_BIND_DN="cn=admin,dc=your,dc=company,dc=com"|g' baw_LDAP.property.3 > baw_LDAP.property.4
-sed -r 's|LDAP_BIND_DN_PASSWORD="<Required>"|LDAP_BIND_DN_PASSWORD="theusmmerofBpmr0cks!"|g' baw_LDAP.property.4 > baw_LDAP.property.5
+sed -r 's|LDAP_BIND_DN_PASSWORD="<Required>"|LDAP_BIND_DN_PASSWORD="thesummerofBpmr0cks!"|g' baw_LDAP.property.4 > baw_LDAP.property.5
 sed -r 's|LDAP_SSL_ENABLED="True"|LDAP_SSL_ENABLED="false"|g' baw_LDAP.property.5 > baw_LDAP.property.6
 sed -r 's|LDAP_USER_NAME_ATTRIBUTE="<Required>"|LDAP_USER_NAME_ATTRIBUTE="*:uid"|g' baw_LDAP.property.6 > baw_LDAP.property.7
 sed -r 's|LDAP_USER_DISPLAY_NAME_ATTR="<Required>"|LDAP_USER_DISPLAY_NAME_ATTR="cn"|g' baw_LDAP.property.7 > baw_LDAP.property.8
