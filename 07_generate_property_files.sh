57717146e542a49f6522275f691755e83b32c869
Gerard Mortel <gmortel@us.ibm.com>
2023-07-28 10:29:02 -0500

    Removed Press any key from orig script

--- 07_generate_property_files.sh
+++ 07_generate_property_files.sh
@@ -6,7 +6,15 @@ echo "=== In 07_generate_property_files.sh =====================================
 echo "================================================================================"
 echo ""
 
+# Get deployment hostname suffix, replace "api" with "apps"
+DEPLOYMENTHOSTNAMESUFFIX=$(sed "s/api/apps/" <<< "$HOSTNAME")
+
 # Generate property files
 cd ibm-cs-bawautomation/inventory/cp4aOperatorSdk/files/deploy/crs/cert-kubernetes/scripts
 ./baw-prerequisites.sh -m generate
-./baw-prerequisites.sh -m generate-cr
+
+# Disable "Press any key to continue" from script
+sed -ri "s/(    read -rsn1 -p\"Press any key to continue\";echo)/#\1/" baw-prerequisites.sh
+
+#./baw-prerequisites.sh -m generate-cr
+printf "%s\n" Yes 2 1 $DEPLOYMENTHOSTNAMESUFFIX 1 | ./baw-prerequisites.sh -m generate-cr
